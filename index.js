ea48fa7f753a5da276cb79a677a08a75abf509ff
anisssoudki <aniss.marrouch@gmail.com>
2020-08-28 09:07:25 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let array = [];
+    tutorial.split(" ").forEach( word => {
+      array.push(word.charAt(0).toUpperCase() + word.slice(1))
+    })
+    return(array.join(" "))
+  })
 }
