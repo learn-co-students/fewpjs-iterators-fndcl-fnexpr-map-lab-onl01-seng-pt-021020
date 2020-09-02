2b58a619172b8dc10e3a2355bd2a1d904205dc17
pbedrosian <peter_bedrosian@yahoo.com>
2020-09-01 21:05:30 -0400

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(sentence => {
+      let words = sentence.split(' ')
+      let capitalizedWord =
+          words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      let response = capitalizedWord.join(' ')
+      return response
+  })
 }
